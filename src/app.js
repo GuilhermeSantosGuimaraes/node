6d0c8513c386d502d3b85b39312b401c96e9c250
guimaraesgui <guimaraes11santos1@gmail.com>
2022-10-10 15:06:25 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
     {id: 1, "titulo": "Senhor dos aneis"},
     {id: 2, "titulo": "O Hobbit"}
@@ -11,8 +13,31 @@ app.get('/', (req, res) => {
     res.status(200).send('Curso de node');
 })
 
+app.get('/livros/:id', (req, res) => {
+    let id = buscarPorId(req.params.id);
+    res.json(livros[id]);
+})
+
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 })
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).json('Livro cadastrado com sucesso!')
+})
+
+app.put('/livros/:id', (req, res) => {
+    let id = buscarPorId(req.params.id);
+    livros[id].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+app.delete('livros/:id', (req, res) => {
+})
+
+function buscarPorId(id){
+    return livros.findIndex(livro => livro.id == id);
+}
 
 export default app;
