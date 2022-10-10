cf7f9bef3da38d57d600ed2812e3f310203ecc1d
guimaraesgui <guimaraes11santos1@gmail.com>
2022-10-10 14:26:56 -0300

    aula2

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-const http = require('http');
-const porta = 3000;
+import app from './src/app.js';
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na pag de livros',
-    '/autores': 'Lista de autores'
-}
+const porta = process.env.PORTA || 3000;
 
-const server = http.createServer((req, res) =>{
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.end(rotas[req.url]);
-})
-
-server.listen(porta, () => {
+app.listen(porta, () => {
     console.log(`Servidor escutando em http://localhost:${porta}`)
 });
